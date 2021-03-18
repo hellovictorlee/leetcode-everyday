214721d1d55e4b241dc0ed4ebdf16db6198faab0
Victor <hellovictorlee@gmail.com>
2021-03-18 14:21:28 -0700

    update

---
+++ 252.meeting_rooms.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals:
+            return True
+        intervals.sort()
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start < prevEnd:
+                return False
+            prevEnd = end
+        return True
