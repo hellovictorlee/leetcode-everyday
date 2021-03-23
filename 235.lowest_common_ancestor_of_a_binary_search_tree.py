05c1453312686bd6578ad8241db545d81a13cb9f
Victor <hellovictorlee@gmail.com>
2021-03-23 15:36:22 -0700

    update

--- 235.lowest_common_ancestor_of_a_binary_search_tree.py
+++ 235.lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,9 +7,6 @@
 
 
 class Solution:
-    def __init__(self):
-        self.ans = None
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
                              q: 'TreeNode') -> 'TreeNode':
         if p.val > q.val:
